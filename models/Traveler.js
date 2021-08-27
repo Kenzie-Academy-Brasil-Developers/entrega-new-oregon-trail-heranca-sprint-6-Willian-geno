30fcc69195782241919a87705536acb2e3764c73
Willian <willian_g_s_s@hotmail.com>
2021-08-26 23:16:39 -0300

    primal commit

--- models/Traveler.js
+++ models/Traveler.js
@@ -1,5 +1,59 @@
 class Traveler {
+    
+    constructor (name,food = 1){
+        this.name = name;
+        this.food = food;
+        this.isHealthy = true;
+    }
 
+    hunt(){
+        this.food += 2;
+    }
+
+    eat(){
+        if(this.food > 0){
+            this.food -= 1;
+        }else{
+            this.isHealthy = false;
+        }
+    }
 }
+
+class Hunter extends Traveler {
+    constructor(name, food = 2,idHealthy){
+        super(name, idHealthy)
+        this.food = food;
+    }
+
+    hunt(){
+        this.food += 4;
+    }
+
+    eat(){
+        if(this.food >= 2){
+            this.food -= 2;
+        }else if(this.food = 1){
+            this.food -= 1;
+            this.isHealthy = false;
+        }else{
+            this.isHealthy = false;
+        }
+    }
+    giveFood(traveler, numOfFoodUnits){
+        if (numOfFoodUnits <= this.food){
+            this.food -= numOfFoodUnits;
+            traveler.food = numOfFoodUnits;
+        }
+    }
+}
+
+class Doctor extends Traveler{
+    
+    heal(traveler){
+        traveler.isHealthy = true;
+    }
+
+} 
+
 
 module.exports = Traveler;
--- models/Wagon.js
+++ models/Wagon.js
@@ -1,5 +1,32 @@
 class Wagon {
-
+    constructor(capacity){
+        this.capacity = capacity,
+        this.passageiros = []
+    }
+    getAvailableSeatCount(){
+        return (this.capacity - this.passageiros.length)
+    }
+    join(passageiro){
+        if (this.passageiros.length < this.capacity){
+            this.passageiros.push(passageiro)
+        }
+    }
+    shouldQuarantine(){
+        for(let i = 0; i < this.passageiros.length; i++){
+            if (!this.passageiros[i].isHealthy){
+                return true;
+            }
+        }
+        return false;
+    }
+    totalFood(){
+        let totalFood = 0;
+        for(let i = 0; i < this.passageiros.length; i++){
+            totalFood += this.passageiros[i].food;
+        }
+        return totalFood;
+    }
 }
+
 
 module.exports = Wagon;
